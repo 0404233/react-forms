c983861b8382812ed9e24e25de736d716694cd11
0404233 <andrey0404233@gmail.com>
2025-08-25 01:08:54 +0300

    docs: clear main branch

--- src/hooks.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { useSubmissions, useCountries } from './hooks';
-
-describe('hooks', () => {
-  it('should export functions', () => {
-    expect(typeof useSubmissions).toBe('function');
-    expect(typeof useCountries).toBe('function');
-  });
-});
--- src/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux'
-import type { TypedUseSelectorHook } from 'react-redux'
-import type { AppDispatch, RootState } from './store'
-
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const useCountries = () => useAppSelector(s => s.countries)
-export const useSubmissions = () => useAppSelector(s => s.submissions)
