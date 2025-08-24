33fe2610c2cf4df2d17b92d88671f1ebc8710c86
0404233 <andrey0404233@gmail.com>
2025-08-24 20:31:19 +0300

    feat: implement tests for image.ts & main.ts

--- src/main.test.tsx
+++ src/main.test.tsx
@@ -1,7 +1,28 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, vi, expect, beforeEach } from 'vitest'
 
-describe('main entry', () => {
-  it('should be defined', () => {
-    expect(true).toBe(true);
-  });
-});
+vi.mock('react-dom/client', async () => {
+  const actual = await vi.importActual<typeof import('react-dom/client')>('react-dom/client')
+  return {
+    ...actual,
+    createRoot: vi.fn(() => ({
+      render: vi.fn()
+    }))
+  }
+})
+
+describe('main.tsx', () => {
+  beforeEach(() => {
+    const root = document.createElement('div')
+    root.id = 'root'
+    document.body.appendChild(root)
+  })
+
+  it('call createRoot', async () => {
+    const { createRoot } = await import('react-dom/client')
+    const rootMock = createRoot(document.getElementById('root')!)
+
+    await import('./main')
+
+    expect(createRoot).toHaveBeenCalled()
+  })
+})
