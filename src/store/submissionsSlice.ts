236e5ccf953219b7dad90549ede8cf935a3e9d3a
0404233 <andrey0404233@gmail.com>
2025-08-25 00:55:07 +0300

    feat: implement tests for submissionsSlice.test

--- src/store/submissionsSlice.ts
+++ src/store/submissionsSlice.ts
@@ -6,23 +6,23 @@ export type Gender = 'male' | 'female'
 
 
 export interface Submission {
-id: string
-name: string
-age: number
-email: string
-password: string
-gender: Gender
-acceptedTC: boolean
-pictureBase64?: string
-country: string
-source: 'uncontrolled' | 'react-hook-form'
-createdAt: number
+  id: string
+  name: string
+  age: number
+  email: string
+  password: string
+  gender: Gender
+  acceptedTC: boolean
+  pictureBase64?: string
+  country: string
+  source: 'uncontrolled' | 'react-hook-form'
+  createdAt: number
 }
 
 
 export interface SubmissionsState {
-items: Submission[]
-lastNewId?: string
+  items: Submission[]
+  lastNewId?: string
 }
 
 
@@ -30,22 +30,22 @@ const initialState: SubmissionsState = { items: [] }
 
 
 const submissionsSlice = createSlice({
-name: 'submissions',
-initialState,
-reducers: {
-addSubmission: {
-prepare(data: Omit<Submission, 'id' | 'createdAt'>) {
-return { payload: { ...data, id: nanoid(), createdAt: Date.now() } }
-},
-reducer(state, action: PayloadAction<Submission>) {
-state.items.unshift(action.payload)
-state.lastNewId = action.payload.id
-}
-},
-clearHighlight(state) {
-state.lastNewId = undefined
-}
-}
+  name: 'submissions',
+  initialState,
+  reducers: {
+    addSubmission: {
+      prepare(data: Omit<Submission, 'id' | 'createdAt'>) {
+        return { payload: { ...data, id: nanoid(), createdAt: Date.now() } }
+      },
+      reducer(state, action: PayloadAction<Submission>) {
+        state.items.unshift(action.payload)
+        state.lastNewId = action.payload.id
+      }
+    },
+    clearHighlight(state) {
+      state.lastNewId = undefined
+    }
+  }
 })
 
 
